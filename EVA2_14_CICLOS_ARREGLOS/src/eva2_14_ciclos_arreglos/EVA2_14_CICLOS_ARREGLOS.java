7b887bff4b49c53ef672dac89be73cb190b43ac8
24550261 <L24550261@chihuahua2.tecnm.mx>
2024-10-22 00:06:08 -0600

    Add files via upload

---
+++ EVA2_14_CICLOS_ARREGLOS/src/eva2_14_ciclos_arreglos/EVA2_14_CICLOS_ARREGLOS.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_14_ciclos_arreglos;
+
+/**
+ *
+ * @author Javis
+ */
+public class EVA2_14_CICLOS_ARREGLOS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+int[] arr = new int[10]; 
+    
+for (int i = 0; i < arr.length; i++){
+arr[i] = (int)(Math.random() * 100);
+
+}
+
+      
+      
+      for (int i = 0; i < arr.length; i++){
+          System.out.print(arr [i] + " - ");
+        
+
+}
+      
+    }
+    }
+    
+
---
+++ EVA2_15_DIAS_SEMANA/src/main/java/com/mycompany/eva2_15_dias_semana/EVA2_15_DIAS_SEMANA.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva2_15_dias_semana;
+
+/**
+ *
+ * @author Javis
+ */
+public class EVA2_15_DIAS_SEMANA {
+
+    public static void main(String[] args) {
+        String[] dS = new String[7];
+dS[0] = "Domingo";
+dS[1] = "Lunes";
+dS[2] = "Martes";
+dS[3] = "Miercoles";
+dS[4] = "Jueves";
+dS[5] = "Viernes";
+dS[6] = "Sabado";
+
+for (int i = 0; i < dS.length; i++){
+    System.out.print(dS[i] + " - ");
+    }
+        
+    }
+}
